7fa5d29b338fe1f48cc31538fd63c4c1c6d5c858
Adedoyin <adebajoa15@gmail.com>
2022-08-31 17:06:44 +0100

    master

---
+++ assignment-2/src/App.js
@@ -0,0 +1,13 @@
+import './basic.css';
+
+function App() {
+  return (
+    <div  id='main'>
+      <div  id='header-class'>
+        <h1>Basic react application</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
